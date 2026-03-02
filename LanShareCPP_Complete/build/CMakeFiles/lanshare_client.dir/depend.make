d04e3032cb1d0eec99392074655ab9edfa319c21
Aditya Dengale <adityadengale16@gmail.com>
2026-03-02 15:57:45 +0530

    Remove build folder from tracking

--- LanShareCPP_Complete/build/CMakeFiles/lanshare_client.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for lanshare_client.
-# This may be replaced when dependencies are built.
--- LanShareCPP_Complete/build/CMakeFiles/lanshare_server.dir/lanshare_server_autogen/mocs_compilation.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/lanshare_server.dir/lanshare_server_autogen/mocs_compilation.cpp.obj: \
- D:\ADITYA\LanShare\ C++\LanShareCPP_Complete\build\lanshare_server_autogen\mocs_compilation.cpp
